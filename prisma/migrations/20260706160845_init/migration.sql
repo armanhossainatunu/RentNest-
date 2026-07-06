b186dd2bc2131bdd4b809efbfd4f7b81f7dd8e5a
Arman Hossain Atunu <armanhossainatunu@gmail.com>
2026-07-07 00:37:59 +0600

    user schema nad profile schema created

---
+++ prisma/migrations/20260706160845_init/migration.sql
@@ -0,0 +1,38 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('TENANT', 'LANDLORD', 'ADMIN');
+
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('ACTIVE', 'INACTIVE');
+
+-- CreateTable
+CREATE TABLE "profiles" (
+    "id" TEXT NOT NULL,
+    "profileProto" TEXT,
+    "bio" TEXT,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "profiles_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "status" "Status" NOT NULL DEFAULT 'ACTIVE',
+    "role" "Role" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "profiles_userId_key" ON "profiles"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "profiles" ADD CONSTRAINT "profiles_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,38 @@
+import cookieParser from "cookie-parser";
 import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import { prisma } from "./lib/prisma";
+import bcrypt from "bcrypt";
+import { config } from "./config";
+
 
 const app: Application = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:5000",
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!");
+});
+
+app.post("/api/auth/register", async(req: Request, res: Response) => {
+  const { name, email, password,profilePhoto,role } = req.body;
+  const isUserExists = await prisma.user.findUnique({
+    where: { email },
+  });
+  if(isUserExists) {
+    throw new Error("User already exists");
+  }
+  const hashedPassword = await bcrypt.hash(password, Number(config.bcrypt_salt_rounds));
+  res.status(201).json({ message: "User registered successfully" });
 });
 
 export default app;
