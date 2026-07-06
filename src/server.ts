4e682ef2d0b8716fdfb60ac6d4153624e70d968d
Arman Hossain Atunu <armanhossainatunu@gmail.com>
2026-07-06 17:36:14 +0600

    data base connected node

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import app from "./app";
+import { prisma } from "./lib/prisma";
 const PORT = process.env.PORT || 5000;
 
 async function main() {
   try {
+    await prisma.$connect();
+    console.log("Connected to the database");
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
