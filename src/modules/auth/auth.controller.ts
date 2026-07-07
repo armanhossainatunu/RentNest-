0d46e98e584e6d37faead5ebe1a4e8f2de0ad6ec
Arman Hossain Atunu <armanhossainatunu@gmail.com>
2026-07-08 01:15:07 +0600

    accessToken and refreshToken set cookie done

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -10,13 +10,27 @@ const loginUser = catchAsync(
 
     const { accessToken, refreshToken } = await authService.loginUser(payload);
 
+    res.cookie("accessToken", accessToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "none",
+      maxAge: 1000 * 60 * 60 * 24,
+    });
+
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "none",
+      maxAge: 1000 * 60 * 60 * 7,
+    });
+
     sendResponse(res, {
       success: true,
       statusCode: httpStatus.OK,
       message: "User logged in successfully",
       data: {
         accessToken,
-        refreshToken
+        refreshToken,
       },
     });
   },
