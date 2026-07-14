8b6d83769d47fb3d0bdc2d8c8297f91ea59e77e3
Arman Hossain Atunu <armanhossainatunu@gmail.com>
2026-07-15 02:59:11 +0600

    some code update

--- src/modules/property/property.route.ts
+++ src/modules/property/property.route.ts
@@ -34,7 +34,7 @@ router.put(
 // update Rental requests
 router.put(
   "/requests/:id",
-  auth(Role.LANDLORD, Role.ADMIN),
+  auth(Role.LANDLORD),
   propertyController.updateRentalRequestStatus,
 );
 // get landlord rental requests
